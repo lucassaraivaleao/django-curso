cdb20f61253c27c224bb7b759977994c12f8a95a
Lucas Saraiva Leao <saraivaleaolucas@gmail.com>
2022-02-26 11:13:31 -0300

    Setando debug

--- cursodjango/settings.py
+++ cursodjango/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9+gmbji9i&xz2k)c#7229#g8d8yn72a01y8knn*b9nytw=h(_q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
