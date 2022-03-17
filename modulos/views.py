f24f111966253961460a429912b40da105726cd4
Lucas Saraiva Leao <saraivaleaolucas@gmail.com>
2022-03-17 01:04:59 -0300

    finishhh

--- modulos/views.py
+++ modulos/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from modulos import facade
+
+
+def indice(request):
+    ctx = {'modulos': facade.listar_modulos_com_aulas()}
+    return render(request, 'modulos/indice.html', ctx)
 
 
 def detalhe(request, slug):
@@ -7,7 +13,7 @@ def detalhe(request, slug):
     aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
 
-
+@login_required
 def aula(request, slug):
     aula = facade.encontar_aula(slug)
     return render(request, 'modulos/aula_detalhe.html', {'aula': aula})
